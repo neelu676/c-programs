9e08332dad96b46aac75e5584d4550b2e32e7150
neelu676 <neelima24c@gmail.com>
2025-07-03 17:14:27 +0530

    Add files via upload

---
+++ The number is positive or negative.cpp
@@ -0,0 +1,12 @@
+ // the number is positive or negative 
+ #include<stdio.h>
+int main()
+{
+	int n=0;
+	if(n>0)
+	printf("positive");
+	else if(n<0)
+	printf("negative");
+	else
+	printf("zero");
+}
