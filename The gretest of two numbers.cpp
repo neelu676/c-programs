9592f2d7930d62f40416eb2f04c27ff5b6c40120
neelu676 <neelima24c@gmail.com>
2025-07-03 17:55:20 +0530

    Add files via upload

---
+++ The gretest of two numbers.cpp
@@ -0,0 +1,12 @@
+ //The gretest of two numbers
+ #include<stdio.h>
+ int main()
+ {
+ 	int n1=9,n2=5;
+ 	if(n1==n2)
+ 	printf("both are equal");
+ 	else if(n1>n2)
+ 	printf("%d is gretest",n1);
+ 	else
+ 	printf("%d is gretest",n2);
+ }
