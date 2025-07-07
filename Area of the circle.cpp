4df6a4aee2ad2df07daa52f8ff3f562fa440e603
neelu676 <neelima24c@gmail.com>
2025-07-07 16:10:43 +0530

    Add files via upload

---
+++ Area of the circle.cpp
@@ -0,0 +1,10 @@
+ // Area of the circle
+ #include<stdio.h>
+ int main()
+ {
+ 	 float r,area,pi=3.14;
+ 	printf("enter  r:");
+ 	scanf("%f",&r);
+ 	area=pi*r*r;
+ 	printf("area of the circle is %2f",area);
+ }
