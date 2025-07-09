3d1cc2b306700f944f940efdbdc6fb08a8dd7c24
neelu676 <neelima24c@gmail.com>
2025-07-09 22:29:42 +0530

    Add files via upload

---
+++ sum of array.cpp
@@ -0,0 +1,16 @@
+ // sum of array
+ #include<stdio.h>
+ int main()
+ {
+ 	int i,n,sum=0;
+ 	int arr[n];
+ 	printf("enter number of elements:");
+ 	scanf("%d",&n);
+ 	printf("enter element in array");
+ 	for(i-0;i<n;i++)
+ 	{
+ 		scanf("%d",&arr[i]);
+ 		sum+=arr[i];
+	 }
+	 printf("sum of elements=%d\n",sum);
+ }
