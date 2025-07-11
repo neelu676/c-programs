62a1a27971d30ab273afac6e88fd416e51bdada5
neelu676 <neelima24c@gmail.com>
2025-07-11 19:38:21 +0530

    Add files via upload

---
+++ vowel or consonent.cpp
@@ -0,0 +1,18 @@
+ // vowel or consonent
+ #include<stdio.h>
+ int main()
+ {
+ 	char c;
+ 	printf("enter character:");
+ 	scanf("%c",&c);
+ 	
+ 	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+ 	{
+ 		printf("vowel");
+ 		
+	 }
+	 else
+	 {
+	 	printf("consonent");
+	 }
+  } 
