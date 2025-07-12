6e3ddce49409dc98b536ea3e367e36f624369169
neelu676 <neelima24c@gmail.com>
2025-07-12 20:21:38 +0530

    Add files via upload

---
+++ without return type without argument.cpp
@@ -0,0 +1,16 @@
+ // without return type without argument
+ #include<stdio.h>
+ void add()
+ {
+ 	int a,b;
+ 	printf("enter a,b:");
+ 	scanf("%d%d",&a,&b);
+ 	int sum=a+b;
+ 	printf("%d",sum);
+ }
+ int main()
+ {
+ 	
+ 	add();
+ }
+ 
