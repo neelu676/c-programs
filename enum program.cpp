b2a993a55acf0eb53a2645473f59b21667ff4927
neelu676 <neelima24c@gmail.com>
2025-07-13 15:37:45 +0530

    Add files via upload

---
+++ enum program.cpp
@@ -0,0 +1,9 @@
+// enum program
+#include<stdio.h>
+enum{SUNDAY,MONDAY,TUESDAY,WENDSDAY,THURSDAY,FRIDAY,SATURDAY};
+int main()
+{
+	int today;
+	today=WENDSDAY;
+	printf("today is WENDSDAY=%d\n",today);
+}
